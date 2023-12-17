8407da87f199d4b6176365d4d0b83452ae87df2e
rulkimi <azrulkimi00@gmail.com>
2023-12-17 20:05:16 +0800

    code cleanup for myskills.vue

--- src/components/sections/MySkills.vue
+++ src/components/sections/MySkills.vue
@@ -35,17 +35,17 @@ export default {
             { name: 'JavaScript', mastery: 'Intermediate', image: require('@/assets/img/logo-javascript.svg') },
             { name: 'Vue Js', mastery: 'Intermediate', image: require('@/assets/img/vue-9.svg') },
             { name: 'C++', mastery: 'Beginner', image: require('@/assets/img/cpp.svg') },
-            { name: 'Python', mastery: 'Beginner', image: require('@/assets/img/python-5.svg') },
+            { name: 'Python', mastery: 'Beginner', image: require('@/assets/img/python-5.svg') }
           ]
-        },
-        { title: 'Languages',
+        }, {
+          title: 'Languages',
           icon: 'ri-pantone-line',
           types: [
             { name: 'Malay', mastery: 'Native Speaker', image: require('@/assets/img/malaysia.svg') },
             { name: 'English', mastery: 'Highly Proficient', image: require('@/assets/img/united-kingdom.svg') },
             { name: 'Mandarin', mastery: 'Conversational (HSK4)', image: require('@/assets/img/china-2.svg') },
             { name: 'Japanese', mastery: 'Conversatioal (N3)', image: require('@/assets/img/japan.jpg') },
-            { name: 'Thai', mastery: 'Beginner', image: require('@/assets/img/thailand.svg') },
+            { name: 'Thai', mastery: 'Beginner', image: require('@/assets/img/thailand.svg') }
           ]
         },
       ]
