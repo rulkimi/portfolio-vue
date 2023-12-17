bbe17661de3b33dcdf27622fd6806b7da14f1626
rulkimi <azrulkimi00@gmail.com>
2023-12-17 16:46:27 +0800

    toggle menu

--- src/components/NavigationsMenu.vue
+++ src/components/NavigationsMenu.vue
@@ -3,28 +3,28 @@
     <a href="#" class="nav__logo">
       Azrul
     </a>
-    <div class="nav__menu" id="nav-menu">
+    <div class="nav__menu" ref="nav-menu">
       <ul class="nav__list grid">
         <li v-for="item in items" class="nav__item">
-          <a :href="item.link" class="nav__link">
+          <a :href="item.link" class="nav__link" @click="toggleMenu">
             <i class="ri-home-5-line"></i> {{ item.title }}
           </a>
         </li>
       </ul>
       <!-- Close button -->
-      <div class="nav__close" id="nav-close">
+      <div class="nav__close" id="nav-close" @click="toggleMenu">
         <i class="ri-close-line"></i>
       </div>
     </div>
     <div class="nav__buttons">
-        <!-- theme change buttons -->
-        <i class="ri-moon-line change-theme" id="theme-button"></i>
+      <!-- theme change buttons -->
+      <i class="ri-moon-line change-theme" id="theme-button"></i>
 
-        <!-- toggle button -->
-        <div class="nav__toggle" id="nav-toggle">
-          <i class="ri-menu-4-line"></i>
-        </div>
+      <!-- toggle button -->
+      <div class="nav__toggle" id="nav-toggle" @click="toggleMenu">
+        <i class="ri-menu-4-line"></i>
       </div>
+    </div>
   </nav>
 </template>
 
@@ -40,6 +40,11 @@ export default {
         { title: 'Projects', link: '#small-projects'},
         { title: 'Contact', link: '#contact'},
       ]
+    }
+  },
+  methods: {
+    toggleMenu() {
+      this.$refs['nav-menu'].classList.toggle('show-menu');
     }
   }
 }
