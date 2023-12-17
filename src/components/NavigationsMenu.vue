ff1ab6a55b769eb878fbcc2ab6245e1710899089
rulkimi <azrulkimi00@gmail.com>
2023-12-17 20:43:26 +0800

    added scroll reveal to the navigations

--- src/components/NavigationsMenu.vue
+++ src/components/NavigationsMenu.vue
@@ -6,7 +6,7 @@
     <div class="nav__menu" ref="nav-menu">
       <ul class="nav__list grid">
         <li v-for="item in items" class="nav__item">
-          <a :href="item.link" class="nav__link" @click="toggleMenu">
+          <a :href="item.link" class="nav__link" @click="toggleMenu" :class="{ 'active-link': isActiveLink(item.link) }">
             <i class="ri-home-5-line"></i> {{ item.title }}
           </a>
         </li>
@@ -39,13 +39,38 @@ export default {
         { title: 'Aspirations', link: '#aspirations'},
         { title: 'Projects', link: '#small-projects'},
         { title: 'Contact', link: '#contact'},
-      ]
+      ],
+      activeLink: '#home', // Add a new data property to track the active link
     }
   },
   methods: {
+    isActiveLink(link) {
+      return this.activeLink === link;
+    },
     toggleMenu() {
       this.$refs['nav-menu'].classList.toggle('show-menu');
-    }
-  }
+    },
+    scrollActive() {
+      const scrollDown = window.scrollY;
+
+      this.items.forEach((item) => {
+        const targetSection = document.querySelector(item.link);
+        if (targetSection) {
+          const sectionHeight = targetSection.offsetHeight,
+                sectionTop = targetSection.offsetTop - 58;
+
+          if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            this.activeLink = item.link;
+          }
+        }
+      });
+    },
+  },
+  mounted() {
+    window.addEventListener('scroll', this.scrollActive);
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.scrollActive);
+  },
 }
 </script>
