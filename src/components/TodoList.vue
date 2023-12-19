a2d592aec3a847d28dad4f3141c4daa14baaede7
rulkimi <azrulkimi00@gmail.com>
2023-12-19 20:30:58 +0800

    added todo list small projects

---
+++ src/components/TodoList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="todo__container">
+    <div class="todo-input-grid">
+      <input placeholder="Todo name" class="name-input" v-model="todoInput">
+      <input type="date" class="due-date-input" v-model="todoDateInput">
+      <button class="add-todo-button" @click="addTodo">Add</button>
+    </div>
+    <div class="todo-grid">
+      <template v-for="(list, index) in todoList" :key="index">
+        <div>{{ list.title }}</div>
+        <div>{{ list.dueDate }}</div>
+        <button class="delete-todo-button" @click="deleteTodo(index)">Delete</button>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  data() {
+    return {
+      todoInput: null,
+      todoDateInput: null,
+      todoList: [
+        { title: 'To do list Example #1', dueDate: '2023-12-22' },
+        { title: 'To do list Example #2', dueDate: '2023-09-22' },
+      ]
+    }
+  },
+  methods: {
+    addTodo() {
+      this.todoList.push({
+        title: this.todoInput,
+        dueDate: this.todoDateInput
+      });
+
+      this.todoInput = null;
+      this.todoDateInput = null;
+    },
+    deleteTodo(index) {
+      this.todoList.splice(index, 1);
+    }
+  }
+}
+</script>
--- src/components/sections/SmallProjects.vue
+++ src/components/sections/SmallProjects.vue
@@ -2,7 +2,7 @@
   <PageSection type="small-projects" title="Small Projects" subtitle="To showcase my works and determination to learn">
     <div v-for="project in projects" :key="project.title" class="small-projects__content">
       <h3 class="small-projects__title">
-        <i :class="project.icon"></i> {{ project.title }}
+        <i :class="project.icon"></i> {{ project.title }} <i :class="project.icon"></i>
       </h3>
       <component :is="project.component" />
     </div>
@@ -15,6 +15,7 @@ import WheelsOfFortune from '../WheelsOfFortune.vue';
 import Calculator from '../Calculator.vue';
 import Stopwatch from '../Stopwatch.vue';
 import TicTacToe from '../TicTacToe.vue';
+import TodoList from '../TodoList.vue';
 
 export default {
   components: {
@@ -22,7 +23,8 @@ export default {
     WheelsOfFortune,
     Calculator,
     Stopwatch,
-    TicTacToe
+    TicTacToe,
+    TodoList
   },
   data() {
     return {
@@ -31,6 +33,7 @@ export default {
         { title: 'Calculator', icon: 'ri-calculator-line', component: 'calculator' },
         { title: 'Stopwatch', icon: 'ri-timer-line', component: 'stopwatch' },
         { title: 'Tic-Tac-Toe', icon: 'ri-artboard-2-line', component: 'tic-tac-toe' },
+        { title: 'Todo List', icon: 'ri-calendar-todo-fill', component: 'todo-list' },
       ],
     };
   },
