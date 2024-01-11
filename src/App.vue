f06f8d60d14376c2946adec5f64b5777a45426d8
rulkimi <azrulkimi00@gmail.com>
2024-01-11 17:41:44 +0800

    remove router

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,45 @@
 <template>
-  <router-view/>
+  <TheHeader />
+  <main class="main">
+    <HomePage />
+    <MySkills />
+    <MyQualifications />
+    <MyAspirations />
+    <SmallProjects />
+    <SchoolProjects />
+    <ContactMe />
+  </main>
 </template>
+
+<script>
+import TheHeader from '@/components/sections/TheHeader.vue';
+import HomePage from '@/components/sections/HomePage.vue';
+import MySkills from '@/components/sections/MySkills.vue';
+import MyQualifications from '@/components/sections/MyQualifications.vue';
+import MyAspirations from '@/components/sections/MyAspirations.vue';
+import SmallProjects from '@/components/sections/SmallProjects.vue';
+import SchoolProjects from '@/components/sections/SchoolProjects.vue';
+import ContactMe from '@/components/sections/ContactMe.vue';
+import { sr } from '@/utils/scroll-reveal';
+
+export default {
+  name: 'HomeView',
+  components: {
+    TheHeader,
+    HomePage,
+    MySkills,
+    MyQualifications,
+    MyAspirations,
+    SmallProjects,
+    SchoolProjects,
+    ContactMe
+  },
+  mounted() {
+    sr.reveal(`.home__data, .projects__container, .testimonial__container, .footer__container`);
+    sr.reveal(`.home__info div`, {delay: 600, origin: 'bottom', interval: 100});
+    sr.reveal(`.skills__content:nth-child(1), .contact__content:nth-child(1)`, {origin: 'left'});
+    sr.reveal(`.skills__content:nth-child(2), .contact__content:nth-child(2)`, {origin: 'right'});
+    sr.reveal(`.qualification__content, .aspirations__card`, {interval: 100});
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 import store from './store'
 
 import '@/assets/styles.css'
@@ -8,5 +7,4 @@ import '@/assets/styles.css'
 const app = createApp(App);
 
 app.use(store);
-app.use(router);
 app.mount('#app');
