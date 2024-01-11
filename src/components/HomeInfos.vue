cf8c3242467fcd8f45dbd4d3dbbf16a8ed19cb8f
rulkimi <azrulkimi00@gmail.com>
2024-01-11 14:58:48 +0800

    updated some infos

--- src/components/HomeInfos.vue
+++ src/components/HomeInfos.vue
@@ -2,7 +2,7 @@
   <div class="home__info">
     <div v-for="info in biodata" :key="info.title">
       <h3 class="home__info-title">{{ info.title }}</h3>
-      <p class="home__info-description">{{ info.description }}</p>
+      <p class="home__info-description" v-html="addLineBreaks(info.description)"></p>
     </div>
   </div>
   <div class="home__info">
@@ -18,15 +18,20 @@ export default {
   data() {
     return {
       biodata: [
-        { title: 'BIOGRAPHY', description: 'Passionate about designing beautiful web interfaces.'},
+        { title: 'BIOGRAPHY', description: 'Self-taught developer passionate about programming languages.'},
         { title: 'CONTACT', description: 'azrulkimi00@gmail.com' },
-        { title: 'PASSION', description: 'Website Design\nForeign Languages\nProgramming' }
+        { title: 'PASSION', description: 'Website Design<br>Foreign Languages<br>Programming' }
       ],
       workInfos: [
         { title: 'YEARS OF EXPERIENCE', number: '01' },
         { title: 'COMPLETED PROJECTS', number: '08+' },
         { title: 'COMPANIES WORKED', number: '03' }
       ]
+    }
+  },
+  methods: {
+    addLineBreaks(text) {
+      return text.replace(/\n/g, '<br>');
     }
   }
 }
