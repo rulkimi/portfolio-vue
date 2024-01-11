35edfbcaa36d2548aaeeb23fb94a7a2a1f3527d8
rulkimi <azrulkimi00@gmail.com>
2024-01-11 18:13:40 +0800

    added resume

--- src/components/sections/HomePage.vue
+++ src/components/sections/HomePage.vue
@@ -14,6 +14,15 @@
           <img src="@/assets/img/shape-wawes.svg" alt="assets/img/shape-circle.svg" class="home__shape-wawes">
           <img src="@/assets/img/shape-circle.svg" alt="" class="home__shape-circle">
         </div>
+        <ul class="home__social">
+          <a href="https://www.linkedin.com/in/azrul-hakimi-75a32420a/" target="_blank" class="home__social-link">
+            <i class="ri-linkedin-box-line"></i>
+          </a>
+          <a href="https://github.com/rulkimi" target="_blank" class="home__social-link">
+            <i class="ri-github-line"></i>
+          </a>
+          <a href="https://github.com/rulkimi/azrul-portfolio/raw/main/azrul-resume.pdf" target="_blank" download="https://github.com/rulkimi/quiz/blob/main/azrul_resume.pdf"><button class="home__resume" role="button">Download Resume</button></a>
+        </ul>
       </div>
       <HomeInfos />
     </div>
