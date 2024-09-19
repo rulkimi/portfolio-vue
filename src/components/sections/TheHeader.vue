0b7f51070455283f038f7731c8b0346d93e8ad0a
rulkimi <azrulkimi00@gmail.com>
2024-09-19 21:23:56 +0800

    add message to direct to new portfolio

--- src/components/sections/TheHeader.vue
+++ src/components/sections/TheHeader.vue
@@ -1,5 +1,11 @@
 <template>
   <header class="header bg-header" id="header">
+    <div class="portfolio-message-container">
+      <div class="portfolio-message">
+        <span>Thank you for visiting my portfolio! I've recently updated my work.</span>
+        <a href="https://rulkimi.github.io/kimi-portfolio" class="portfolio-button">Go to New Portfolio</a>
+      </div>
+    </div>
     <NavigationsMenu />
   </header>
 </template>
@@ -29,3 +35,43 @@ export default {
 }
 </script>
 
+<style scoped>
+.portfolio-message-container {
+  background-color: #f0f8f8;
+  padding: 16px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.portfolio-message {
+    max-width: 1200px;
+    text-align: center;
+    font-family: 'Arial', sans-serif;
+    color: #333;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    column-gap: 20px;
+}
+
+.portfolio-message p {
+    font-size: 1.1em;
+    margin-bottom: 15px;
+}
+
+.portfolio-button {
+    display: inline-block;
+    background-color: #2aa397;
+    color: #fff;
+    text-decoration: none;
+    padding: 8px 16px;
+    border-radius: 5px;
+    font-size: 1em;
+    transition: background-color 0.3s ease;
+}
+
+.portfolio-button:hover {
+    background-color: #2aa397ce;
+}
+</style>
